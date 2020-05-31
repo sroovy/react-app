ebdc87503b33256ca316afc9e259033c870bbba9
sroovy <42996064+sroovy@users.noreply.github.com>
2020-05-31 20:34:44 +0900

    Change class to Hooks

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,58 +1,40 @@
-// 분할된 파일을 만들 때 필요로하는 패키지나 라이브러리를 가지고 온다. 
 const React = require('react');
-const { Component } = React;
+const { useState } = require('react');
 
-class WordRelay extends Component {
-    state = {
-        word: "수연",
-        value: '',
-        result: '',
-    };
+const WordRelay = () => {
+  const [word, setWord] = useState('수연');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputEl = React.useRef(null);
 
-    onSubmitForm = (e) => {
-        e.preventDefault();
-        if(this.state.word[this.state.word.length - 1] === this.state.value[0]){
-            this.setState({
-                word: this.state.value,
-                value: '',
-                result: '정답입니다',
-            })
-            this.input.focus();
-        } else {
-            this.setState({
-                value: '',
-                result: '땡!!'
-            })
-            this.input.focus();
-        }
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
+      inputEl.current.focus();
+    } else {
+      setResult('땡');
+      setValue('');
+      inputEl.current.focus();
     }
+  };
 
-    input;
-
-    onRefInput = (c) => {
-        this.input = c;
-    }
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.currentTarget.value
-        })
-    }
-
- 
-    render () {
-        return (
-            <div id="word__relay">
-                <div>{this.state.word}</div>
-                <form onSubmit={this.onSubmitForm}>
-                    <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}></input>
-                    {/* value 와 onChange는 set / defaultValue = {this.state.value} */}
-                    <button>Click!</button>
-                </form>
-                <div>{this.state.result}</div>
-            </div>
-        )
-    }
-}
-
-// 분할된 파일을 외부에서도 사용할 수 있게 해주는 코드
-module.exports = WordRelay; 
+  return (
+    <>
+        <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <label id="label" htmlFor="wordInput">입력하세요</label>
+                <input id="wordInput" className="wordInput"
+                ref={inputEl}
+                value={value}
+                onChange={(e) => setValue(e.currentTarget.value)}
+                />
+                <button>입력!</button>
+            </form>
+        <div>{result}</div>
+    </>
+  );
+};
+module.exports = WordRelay;
