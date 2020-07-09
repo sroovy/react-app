ef796ba204ed8b829520fc0a56c826c99223cd22
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-09 11:19:07 +0900

    Add Link to Component
    1. a태그와 같은 기능을 하는 Link 컴포넌트로 연결시킨다. 다른 페이지로 넘어가는 것이 아니라 Route 부분을 불러주는 역할을 한다.

--- 9. Router/Games.jsx
+++ 9. Router/Games.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, HashRouter, Route, Link } from 'react-router-dom';
+import NumberBaseball from '../3. numberbaseball/NumberBaseballHooks';
+import RPS from '../5. RPS/RPShooks';
+import Lotto from '../6. Lotto/LottoHooks';
 
 const Games = () => {
     return (
         <BrowserRouter>
+            {/* 공통인 부분 : 페이지가 바껴도 변하지 않는다.  */}
             <div>
-                <Route path="/number-baseball" component={NumberBaseballHooks}/>
-                <Route path="/rock-scissors-paper" component={RPShooks}/>
-                <Route path="/lottery" component={LottoHooks}/>
+                <Link to ="/number-baseball">숫자야구</Link><br />
+                <Link to ="/rock-scissors-paper">가위바위보</Link><br />
+                <Link to ="/lottery">로또 생성기</Link>
+            </div>
+            {/* 화면이 바뀌는 부분 */}
+            <div>
+                <Route path="/number-baseball" component={NumberBaseball}/>
+                <Route path="/rock-scissors-paper" component={RPS}/>
+                <Route path="/lottery" component={Lotto}/>
             </div>
         </BrowserRouter>
     )
--- 9. Router/README.md
+++ 9. Router/README.md
@@ -38,6 +38,7 @@ ReactDom.render(<BrowserRouter><Hot /></BrowserRouter>, document.querySelector("
 const Games = () => {
     return (
         <BrowserRouter>
+            <Link >
             <div>
                 <Route path="/number-baseball" component={NumberBaseballHooks}/>
                 <Route path="/rock-scissors-paper" component={RPShooks}/>
@@ -51,4 +52,24 @@ const Games = () => {
 리액트 라우터가 여러개 페이지를 동시에 렌더링을 해준다. 
 
 
+
+2. 링크와 브라우저 라우터
+** 리액트 라우터는 눈속임이다...
+페이지가 여러개 있는것처럼 보이는것. 
+
+a태그와 같은 기능을 하는
+Link to 로 연결 
+다른 페이지로 넘어가는 게 아니라 Route 부분을 불러주는 역할
+
+
+http://localhost:8080/number-baseball 에서 새로고침을 하면
+Cannot GET /number-baseball 문구가 뜬다 
+-> 실제로는 페이지가 여러개 있지 않다는 증거 -> 
+
+새로고침을 하거나 주소창에 주소를 입력하는 것은
+서버쪽에 요청을 보내는 일이다. 
+따라서 서버에서는 응답 X -> 에러 문구 
+
+프론트엔드인 리액트 라우터만 알고 있다. 
+-> client side rendering
 
